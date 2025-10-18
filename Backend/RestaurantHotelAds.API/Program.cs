af976e0f02a5a34e3e9a08905a45103c5fa139a5
Noureddin94 <186416386+Noureddin94@users.noreply.github.com>
2025-10-18 14:05:02 +0200

    Adding connection options with frontend not tested yet

--- Backend/RestaurantHotelAds.API/Program.cs
+++ Backend/RestaurantHotelAds.API/Program.cs
@@ -1,5 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:4200",    // hotel-portal
+                "http://localhost:4201",    // restaurant-portal
+                "http://localhost:4202"     // room-display
+            )
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+}
+);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
